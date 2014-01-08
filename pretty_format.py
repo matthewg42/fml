b638e2cbaf8c55fd6de27533e2d393969eb0bc61
Matthew Gates <matthewg42@gmail.com>
2014-01-08 10:44:52 +0000

    sweet-ass formatter class

---
+++ pretty_format.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import re
+
+class PrettyFormat:
+    def __init__(self, width, align='left', fmt='s', pad=''):
+        """ If you want to specify precision for floats, prefix that on the fmt, without the width
+            i.e. PrettyFormat(width=10, fmt='.4f') -> %10.4f
+        """
+        if align not in ['left','right']:
+            raise ValueError("align must be 'left' or 'right'")
+        if re.match('^((\.\d+)?f)|[sd]$', fmt) == None:
+            raise ValueError("fmt must be 's', 'd', 'f', or '.nf' (where n in an int)")
+        if pad not in ['','0']:
+            raise ValueError("pad must be '' or '0'")
+        if not isinstance(width, int):
+            raise ValueError('width must be an int')
+        self.width = width
+        self.align = align
+        self.fmt = fmt
+        self.pad = pad
+
+    def __repr__(self):
+        return "PrettyFormat(width=%d, align='%s', fmt='%s', pad='%s')" % (self.width, self.align, self.fmt, self.pad)
+
+    def fmtstr(self, string=False):
+        """ returns a printf style format string for this PrettyFormat, 
+            If string=True then return as 's' format with the proper width and alignment instead
+            e.g. width=10, align='left', fmt='d', pad='0', string=False -> '%-010d'
+            e.g. width=10, align='left', fmt='d', pad='0', string=True -> '%-10s'
+        """
+        fmt = self.fmt
+        pad = self.pad
+        align = {'left':'-','right':''}[self.align]
+        if string:
+            fmt = 's'
+            pad = ''
+        return '%%%s%s%d%s'  % (align, pad, self.width, fmt)
+
+    def out(self, value, string=False):
+        return self.fmtstr(string) % value
+
+    def underline(self, char='-'):
+        return char * self.width
+
+if __name__ == '__main__':
+    
+    for f in [PrettyFormat(10),
+              PrettyFormat(6,  align='right', fmt='d', pad='0'),
+              PrettyFormat(10, fmt='.3f', pad=''),
+              PrettyFormat(10, align='right', fmt='.3f', pad=''),
+              PrettyFormat(8,  align='right', fmt='d', pad='0') ]:
+        print "f is: " + repr(f)
+        print "f.fmtstr():            '%s'" % f.fmtstr()
+        print "f.fmtstr(string=True): '%s'" % f.fmtstr(True)
+        print "f.out(3):              '%s'" % f.out(3)
+        print "f.out(3):              '%s'" % f.out(300)
+        print "f.out(3):              '%s'" % f.out(123456)
+        print "f.underline():         '%s'" % f.underline()
+        print ""
+
+
+
